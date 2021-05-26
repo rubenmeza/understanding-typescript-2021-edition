697742c21aaafb46f5ec04acb81ed62ddef08a8a
Ruben Meza <rmezar@gmail.com>
2021-05-26 11:02:14 -0500

    add project class and custom type for status and listener

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -1,7 +1,19 @@
+// class Project
+enum ProjectStatus {
+  Active,
+  finished
+}
+
+class Project {
+  constructor (public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {}
+}
+
 // Project State Managment
+type Listener = (items: Project[]) => void
+
 class ProjectState {
-  private listeners: any[] = []
-  private projects: any[] = []
+  private listeners: Listener[] = []
+  private projects: Project[] = []
   private static instance: ProjectState
 
   private constructor () {}
@@ -14,17 +26,12 @@ class ProjectState {
     return this.instance
   }
 
-  addListener(lisnerFn: Function) {
+  addListener(lisnerFn: Listener) {
     this.listeners.push(lisnerFn)
   }
 
   addProject (title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople
-    }
+    const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active)
     this.projects.push(newProject)
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice())
@@ -87,7 +94,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLElement
-  assignedProjects: any[]
+  assignedProjects: Project[]
 
   constructor (private type: 'active' | 'finished') {
     this.assignedProjects = []
@@ -100,7 +107,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement
     this.element.id = `${this.type}-projects`
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects
       this.renderProjects()
     })
